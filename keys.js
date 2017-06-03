f9471a26ed9b1bb0b51fcf7f8ab1187b84450cf0
Rebecca Renavitz <rlrenavitz@gmail.com>
2017-06-02 20:44:28 -0400

    Initial commit: liri.js, keys.js (with twitter api keys), and gitignore.txt added

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<BFShT1exJUt40KT8u7c3TgZfa>',
+  consumer_secret: '<Yz7wfvTkWTsyHl0wdJprvEsz4j8gR8CBrP32O81tus1e4rmhxG>',
+  access_token_key: '<870799033790586881-9KVgHa4zFS9R96NghztIg81CgfBVwyB>',
+  access_token_secret: '<4Vb8h6vXvQwtIGaHSkBo9HpbaanYOwOZqbFGwCBaPA6ab>',
+}
